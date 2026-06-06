96216678159e23545823fd45b741d83c9c4e7ed5
ivan_vot306 <asuste85@ya.ru>
2026-06-06 12:25:16 +0300

    ws

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
 from fastapi import HTTPException
+from starlette import status
+
+from backend.utils import verify_password, create_access_token
 from deps import get_session
 from models import User, UserCreate, UserResponse
 
