afaa62d9f0d3ca97c406d2c6da3eb557032bc1c2
ZuzAser80 <dde4th@gmail.com>
2026-06-06 14:02:47 +0500

    auth login added

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,13 @@
+from tokenize import Token
+
 from fastapi import Depends, FastAPI
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
 from fastapi import HTTPException
+from starlette import status
+
+from backend.utils import verify_password, create_access_token
 from deps import get_session
 from models import User, UserCreate, UserResponse
 
@@ -32,6 +37,16 @@ async def create_user(
             status_code=400,
             detail="Пользователь с таким email уже есть в базе"
         )
+
+@app.post("/login", response_model=Token)
+async def login(data: UserCreate, db: AsyncSession = Depends(get_session())):
+    result = await db.execute(select(User).where(User.username == data.username))
+    user = result.scalar_one_or_none()
+    if not user or not verify_password(data.password, user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    token = create_access_token({"sub": str(user.id)})
+    return Token(access_token=token)
+
 
 @app.get("/users/", response_model=list[UserResponse])
 async def list_users(session: AsyncSession = Depends(get_session)):
